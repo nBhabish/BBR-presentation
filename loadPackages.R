0f46905584b9d76c8be24f4487dd07d767c88a7d
nbhabish <bhabishya.neupane@huskers.unl.edu>
2021-03-31 21:52:09 -0500

    presentation

---
+++ loadPackages.R
@@ -0,0 +1,8 @@
+
+# Loading configurations --------------------------------------------------
+
+install.packages("pacman")
+pacman::p_load(xaringan,
+               xaringanthemer)
+
+remotes::install_github("gadenbuie/xaringanExtra")
